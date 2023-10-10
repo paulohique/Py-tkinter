68c0d21c01f4c7c95e76691082be34bd17b595b4
paulohique <paulohenriquebq2011@hotmail.com>
2023-10-10 16:40:11 -0300

    att
    1.2

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import tkinter as tk
+from tkinter import ttk, messagebox
 
-# Função para exibir a mensagem com base no número inserido
-def exibir_mensagem():
+# Função para exibir um aviso
+def mostrar_aviso():
     numero_inserido = entry_numero.get()
     try:
         numero = int(numero_inserido)
@@ -10,7 +11,7 @@ def exibir_mensagem():
         elif 8 <= numero <= 10:
             mensagem = "Será que minha gata vai me dar um bom dia?"
         elif 11 <= numero <= 12:
-            mensagem = "To almoçando mas queria estar rangando ela!"
+            mensagem = "To almoçando, mas queria estar rangando ela!"
         elif 13 <= numero <= 15:
             mensagem = "Meu momo está trabalhando, sdds dela!"
         elif 16 <= numero <= 18:
@@ -23,35 +24,46 @@ def exibir_mensagem():
             mensagem = "Minha princesa é uma gostosa, podia estar grudadinho nela, quem sabe até dentro rsrs!"
         else:
             mensagem = "Quem me dera se meu dia tivesse mais horas para poder passar com ela!"
-        
-        # Atualiza a mensagem no canto superior direito
-        label_mensagem.config(text=mensagem)
+
+        # Exibe um aviso com a mensagem
+        messagebox.showinfo("Pensamento", mensagem)
     except ValueError:
-        label_mensagem.config(text="Insira um número válido.")
+        messagebox.showerror("Erro", "Insira um horário válido.")
 
 # Cria uma instância da janela principal
 janela = tk.Tk()
-janela.title("Relogio do pensamento no meu amor")
+janela.title("Relogio do pensamento em meu amor")
+# Define a largura e altura da janela
+largura_janela = 200
+altura_janela = 210
+
+# Calcula a posição X e Y para centralizar a janela na tela
+posicao_x = (janela.winfo_screenwidth() // 2) - (largura_janela // 2)
+posicao_y = (janela.winfo_screenheight() // 2) - (altura_janela // 2)
+
+# Define a geometria da janela com largura, altura e posição
+janela.geometry(f"{largura_janela}x{altura_janela}+{posicao_x}+{posicao_y}")
+
 
 # Cria um Entry para inserir o número
 entry_numero = tk.Entry(janela)
 entry_numero.pack(pady=10)
 
-# Cria um botão para exibir a mensagem
-botao_exibir = tk.Button(janela, text="Verificar a hora", command=exibir_mensagem)
-botao_exibir.pack()
-
-# Cria um rótulo para exibir a mensagem no canto superior direito
 label_mensagem = tk.Label(janela, text="", anchor="e", padx=10)
 label_mensagem.pack(side="top", fill="x")
 
-# Define uma mensagem inicial no canto superior direito
-mensagem_inicial = "Bem-vindo! Aqui você sabe os tipos de pensamentos que eu tenho por meu AMOR!"
+# Define uma mensagem inicial no canto superior direito (opcional)
+mensagem_inicial = "Bem-vindo(a)! Aqui você sabe\n os tipos de pensamentos no dia\n que eu tenho com meu AMOR!"
 label_mensagem.config(text=mensagem_inicial)
-label_mensagem.pack(side="top", fill="x")
+label_mensagem.pack(padx= 10, pady=10)
+
+
+# Cria um botão para exibir o aviso
+botao_exibir = ttk.Button(janela, text="Verificar a hora", command=mostrar_aviso)
+botao_exibir.pack()
 
 # Cria um botão "Sair" para fechar a aplicação
-botao_sair = tk.Button(janela, text="Sair", command=janela.quit)
+botao_sair = ttk.Button(janela, text="Sair", command=janela.quit)
 botao_sair.pack()
 
 # Inicia o loop principal da interface gráfica
